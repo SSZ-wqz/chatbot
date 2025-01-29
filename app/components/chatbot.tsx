690d5aaa3bf4430bc67b2bf75f28c50a33494cb7
ShenweiZ <zhangshenwei2004@126.com>
2025-01-29 10:22:50 -0600

    1/29/10.22

---
+++ app/components/chatbot.tsx
@@ -0,0 +1,13 @@
+import Chat from "./chat";
+import View from "./view";
+
+const Chatbot = () => {
+  return (
+    <div>
+      <View />
+      <Chat />
+    </div>
+  );
+};
+
+export default Chatbot;
